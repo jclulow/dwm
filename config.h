c7afa588aee1cbed15d20f5f1f61e6c30603233a
Joshua M. Clulow <josh@sysmgr.org>
2012-01-10 12:01:28 +1100

    audacious jump box should float, dont screw with firefox

--- config.h
+++ config.h
@@ -26,8 +26,8 @@ static unsigned int tagset[] = {1, 1}; /* after start, first tag is selected */
 
 static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            True,        -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       False,       -1 },
+	{ "Gimp",      NULL,       NULL,       0,            True,        -1 },
+	{ "Audacious", NULL,       NULL,       0,            True,        -1 },
 	{ "Gnome-panel",  NULL,       NULL,       1 << 7,       True,       -1 },
 };
 
